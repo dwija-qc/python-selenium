5c6a3ab5958aab9d93619b2c9e6df6a7755f273c
dwija_qc <dwija.sindata@gmail.com>
2022-07-27 16:52:23 +0800

    Tugas15

--- Tugas_ke-15_27_Juli_2022.py
+++ Tugas_ke-15_27_Juli_2022.py
@@ -9,44 +9,142 @@ class tugas(unittest.TestCase):
     def setUp(self): 
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
+    #1 Register Sucess (Positive)
     def test_a_success_register(self): 
         # steps
         browser = self.browser #buka web browser
         browser.get("https://myappventure.herokuapp.com/registration") # buka situs
         time.sleep(3)
-        browser.find_element(By.NAME,"username").send_keys("dwi31") # isi username
+        browser.find_element(By.NAME,"username").send_keys("dwi55") # isi username
         time.sleep(1)
-        browser.find_element(By.NAME,"email").send_keys("dwi31@gmail.com") # isi email
+        browser.find_element(By.NAME,"email").send_keys("dwi55@gmail.com") # isi email
         time.sleep(1)
         browser.find_element(By.NAME,"password").send_keys("dwi123")   # isi password
         time.sleep(1)
         browser.find_element(By.XPATH, '//button[text()="Daftar sekarang"]').click() # klik tombol daftar
-        time.sleep(1)
+        time.sleep(3)
 
         # validasi
-        response_data = browser.find_element(By.ID,"__next").text
+        response_data = browser.find_element(By.CSS_SELECTOR,"p.mt-12.pb-3.max-w-md.font-semibold.text-center").text
+
+        self.assertEquals(response_data,'Selamat! Akun anda berhasil dibuat')
+
+    #2 Register Existing (Negative)
+    def test_b_register_existing(self): 
+
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("https://myappventure.herokuapp.com/registration") # buka situs
+        time.sleep(3)
+        browser.find_element(By.NAME,"username").send_keys("dwi50") # isi username
+        time.sleep(1)
+        browser.find_element(By.NAME,"email").send_keys("dwi50@gmail.com") # isi email
+        time.sleep(1)
+        browser.find_element(By.NAME,"password").send_keys("dwi123")   # isi password
+        time.sleep(1)
+        browser.find_element(By.XPATH, '//button[text()="Daftar sekarang"]').click() # klik tombol daftar
+        time.sleep(3)
+
+        # validasi
+        response_data = browser.find_element(By.CSS_SELECTOR,"p.px-1.leading-5").text
 
 
-        self.assertIn('Selamat', response_data)
+        self.assertEquals(response_data,'Email atau username sudah terdaftar')
 
-    def test_a_login_success(self):
+    #3 Register null password (Negative)
+    def test_c_register_null_password(self): 
+
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("https://myappventure.herokuapp.com/registration") # buka situs
+        time.sleep(3)
+        browser.find_element(By.NAME,"username").send_keys("dwi88") # isi username
+        time.sleep(1)
+        browser.find_element(By.NAME,"email").send_keys("dwi88@gmail.com") # isi email
+        time.sleep(1)
+        browser.find_element(By.NAME,"password").send_keys("")   # isi password
+        time.sleep(1)
+        browser.find_element(By.XPATH, '//button[text()="Daftar sekarang"]').click() # klik tombol daftar
+        time.sleep(3)
+
+        # validasi
+        response_data = browser.find_element(By.CSS_SELECTOR,"div.flex.items-center.justify-start.text-xs.text-white.font-light").text
+
+
+        self.assertEquals(response_data,'diperlukan kata sandi')
+
+    #4 Login Success (Positive)
+    def test_d_login_success(self):
          
          # steps
          browser = self.browser
          browser.get("https://myappventure.herokuapp.com/login")
          time.sleep(3)
-         browser.find_element(By.NAME,"email").send_keys("dwi31@gmail.com")
+         browser.find_element(By.NAME,"email").send_keys("dwi50@gmail.com")
          time.sleep(1)
          browser.find_element(By.NAME,"password").send_keys("dwi123")
          time.sleep(1)
-         browser.find_element(By.TAG_NAME,"button").click()
-
          browser.find_element(By.XPATH, '//button[text()="Masuk"]').click()
+         time.sleep(8)
+
+         response_data = browser.find_element(By.CSS_SELECTOR,"div.flex.justify-center.text-center.text-shadow-xl").text
+
+         self.assertEqual(response_data,'Mulai Petualanganmu')
+
+    
+    #5 Login wrong email (Negative)
+    def test_e_login_wrong_email(self):
+         
+         # steps
+         browser = self.browser
+         browser.get("https://myappventure.herokuapp.com/login")
+         time.sleep(3)
+         browser.find_element(By.NAME,"email").send_keys("dwi99@gmail.com")
          time.sleep(1)
+         browser.find_element(By.NAME,"password").send_keys("dwi123")
+         time.sleep(1)
+         browser.find_element(By.XPATH, '//button[text()="Masuk"]').click()
+         time.sleep(5)
 
-         response_data = browser.find_element(By.ID,"__next")
+         response_data = browser.find_element(By.CSS_SELECTOR,"p.px-1.leading-5").text
 
-         self.assertIn('Anda', response_data)
+         self.assertIn('Alamat email atau kata sandi', response_data)
+
+    #6 Login Wrong Password (Negative)
+    def test_f_login_wrong_password(self):
+         
+         # steps
+         browser = self.browser
+         browser.get("https://myappventure.herokuapp.com/login")
+         time.sleep(3)
+         browser.find_element(By.NAME,"email").send_keys("dwi50@gmail.com")
+         time.sleep(1)
+         browser.find_element(By.NAME,"password").send_keys("dwi1231")
+         time.sleep(1)
+         browser.find_element(By.XPATH, '//button[text()="Masuk"]').click()
+         time.sleep(5)
+
+         response_data = browser.find_element(By.CSS_SELECTOR,"p.px-1.leading-5").text
+
+         self.assertIn('Kata Sandi Salah', response_data)
+
+    #7 Login Null Password (Negative)
+    def test_g_login_pasword_null(self):
+         
+         # steps
+         browser = self.browser
+         browser.get("https://myappventure.herokuapp.com/login")
+         time.sleep(3)
+         browser.find_element(By.NAME,"email").send_keys("dwi50@gmail.com")
+         time.sleep(1)
+         browser.find_element(By.NAME,"password").send_keys("")
+         time.sleep(1)
+         browser.find_element(By.XPATH, '//button[text()="Masuk"]').click()
+         time.sleep(5)
+
+         response_data = browser.find_element(By.CSS_SELECTOR,"div.flex.items-center.justify-start.text-xs.text-white.font-light").text
+
+         self.assertIn('diperlukan kata sandi', response_data)
 
     def tearDown(self): 
         self.browser.close() 
